74d48dacb6b36e7bccabf528bcd25469331ad5ab
robertorocha828 <robertorocha828@gmail.com>
2026-06-22 12:13:34 -0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filter';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express'; // 👈 Importación necesaria
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-  app.useGlobalFilters(new GlobalHttpExceptionFilter());
-  await app.listen(process.env.PORT ?? 3000);
+ const app = await NestFactory.create<NestExpressApplication>(AppModule); // 👈 Tipo específico
+ app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+ app.useGlobalFilters(new GlobalHttpExceptionFilter());
+ app.useStaticAssets(join(__dirname, '..', 'public')); // 👈 Habilita acceso público a /public
+
+
+ const config = new DocumentBuilder()
+   .setTitle('Mi API')
+   .setDescription('Documentación de la API')
+   .setVersion('1.0')
+   .addBearerAuth() // opcional si usas JWT
+   .build();
+ const document = SwaggerModule.createDocument(app, config);
+ SwaggerModule.setup('docs', app, document);
+  await app.listen(3000);
 }
 bootstrap();
