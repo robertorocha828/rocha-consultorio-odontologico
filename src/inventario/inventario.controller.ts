dc9d305a64cb0ad13a59a3612f45cf35a06b49cc
nothingkrisptes <iancm0168@gmail.com>
2026-07-22 23:04:26 -0500

    fix: seguridad en los modulos tratamientos, tipo-tratamientos y inventario

--- src/inventario/inventario.controller.ts
+++ src/inventario/inventario.controller.ts
@@ -2,26 +2,31 @@ import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from 
 import { InventarioService } from './inventario.service';
 import { CreateInventarioDto } from './dto/create-inventario.dto';
 import { UpdateInventarioDto } from './dto/update-inventario.dto';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Controller('inventario')
 export class InventarioController {
   constructor(private readonly inventarioService: InventarioService) {}
 
+  @Roles('admin')
   @Post()
   create(@Body() dto: CreateInventarioDto) {
     return this.inventarioService.create(dto);
   }
 
+  @Roles('admin')
   @Get()
   findAll() {
     return this.inventarioService.findAll();
   }
 
+  @Roles('admin')
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.inventarioService.findOne(id);
   }
 
+  @Roles('admin')
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -30,6 +35,7 @@ export class InventarioController {
     return this.inventarioService.update(id, dto);
   }
 
+  @Roles('admin')
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.inventarioService.remove(id);
--- src/tratamientos/tratamientos.controller.ts
+++ src/tratamientos/tratamientos.controller.ts
@@ -2,26 +2,31 @@ import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from 
 import { TratamientosService } from './tratamientos.service';
 import { CreateTratamientoDto } from './dto/create-tratamiento.dto';
 import { UpdateTratamientoDto } from './dto/update-tratamiento.dto';
+import { Roles } from '../auth/decorators/roles.decorator';
 
 @Controller('tratamientos')
 export class TratamientosController {
   constructor(private readonly tratamientosService: TratamientosService) {}
 
+  @Roles('admin')
   @Post()
   create(@Body() dto: CreateTratamientoDto) {
     return this.tratamientosService.create(dto);
   }
 
+  @Roles('admin')
   @Get()
   findAll() {
     return this.tratamientosService.findAll();
   }
 
+  @Roles('admin')
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.tratamientosService.findOne(id);
   }
 
+  @Roles('admin')
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -30,6 +35,7 @@ export class TratamientosController {
     return this.tratamientosService.update(id, dto);
   }
 
+  @Roles('admin')
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.tratamientosService.remove(id);
