7e5c91767734badf9141ce504a83a0bc1ad7f99f
nothingkrisptes <iancm0168@gmail.com>
2026-07-23 16:15:28 -0500

    fix: citas y horarios

--- src/citas/citas.module.ts
+++ src/citas/citas.module.ts
@@ -5,12 +5,14 @@ import { CitasService } from './citas.service';
 import { Cita } from './cita.entity';
 import { MailModule } from '../mail/mail.module';
 import { NotificacionesModule } from '../notificaciones/notificaciones.module';
+import { HorariosModule } from '../horarios/horarios.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Cita]),
     MailModule,
     NotificacionesModule,
+    HorariosModule,
   ],
   controllers: [CitasController],
   providers: [CitasService],
